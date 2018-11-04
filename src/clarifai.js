b53017cc7f058c7e3f95ec3458a45fab35815860
alanbrilliant <abrilliant2017@gmail.com>
2018-11-03 18:17:38 -0700

    THis will make old men smile

---
+++ src/clarifai.js
@@ -0,0 +1,7 @@
+const Clarifai = require('clarifai');
+
+// initialize with your api key. This will also work in your browser via http://browserify.org/
+
+export const appclar = new Clarifai.App({
+ apiKey: '0d6f43a4849f41f7ada6f995687ac6e5'
+});
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,45 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your cool ass shit App which has been modified"/>
+    <!--  -->
+    <input v-model="picLink" type="text"> <button v-on:click="this.yo">Update</button> <br>
+    <img alt="Vue logo" v-bind:src="this.picLink">
+    <HelloWorld v-bind:msg="this.datassdata[0].name.toString() +': '+ (this.datassdata[0].value*100).toString()+'% accuracy'"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue';
+import {appclar} from '@/clarifai.js';
 
 export default {
   name: 'home',
   components: {
     HelloWorld,
   },
+
+  data() {
+    return {
+      message: 'hrllo',
+      datassdata: [],
+      picLink: 'https://pbs.twimg.com/profile_images/378800000021016777/1d530fa4b92b883270c5bdd7583ed073_400x400.jpeg'
+    };
+  },
+  methods: {
+     yo : function (){
+      this.datassdata = [];
+      appclar.models.initModel({id: Clarifai.GENERAL_MODEL, version: "aa7f35c01e0642fda5cf400f543e7c40"})
+      .then(generalModel => {
+        return generalModel.predict(this.picLink);
+      })
+      .then(response => {
+        response['outputs'][0]['data']['concepts'].forEach(element => {
+          this.datassdata.push(element);
+        });
+      })
+    }
+  },beforeMount(){
+    this.yo();
+  }
 };
 </script>
